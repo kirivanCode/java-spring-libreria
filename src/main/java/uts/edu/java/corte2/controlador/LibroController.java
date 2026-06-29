a69cb4ebd426b3894619bec52d25db7fe4c228b0
kirivanCode <123826376+kirivanCode@users.noreply.github.com>
2026-06-29 15:36:33 -0500

    Refactor LibroController return statements

--- src/main/java/uts/edu/java/corte2/controlador/LibroController.java
+++ src/main/java/uts/edu/java/corte2/controlador/LibroController.java
@@ -25,46 +25,80 @@ public class LibroController {
     @Autowired
     private LibroService libroService;
 
+
     @GetMapping("/")
     public String verLibro(Model model) {
+
         List<Libro> listalibro = libroService.listadoxd();
-       
+
         model.addAttribute("listalibro", listalibro);
-        return "/views/libro/libro";
+
+        return "views/libro/libro";
     }
+
 
     @GetMapping("/new")
     public String mostrarPaginaNuevoLibro(Model model) {
+
         Libro libro = new Libro();
+
         model.addAttribute("libro", libro);
-        return "/views/libro/nuevo_libro";
+
+        return "views/libro/nuevo_libro";
     }
 
+
     @PostMapping("/save")
-    public String saveLibro(@Valid @ModelAttribute("libro") Libro libro, Errors errors, Model model,
-                             RedirectAttributes attribute) {
+    public String saveLibro(
+            @Valid @ModelAttribute("libro") Libro libro,
+            Errors errors,
+            RedirectAttributes attribute) {
+
+
         if (errors.hasErrors()) {
-            return "/views/libro/nuevo_libro";
+            return "views/libro/nuevo_libro";
         }
+
+
         libroService.save(libro);
+
         attribute.addFlashAttribute("success", "Libro guardado con éxito");
+
+
         return "redirect:/views/libro/";
     }
 
+
     @GetMapping("/listar/{id_libro}")
     public String listarId(@PathVariable int id_libro, Model model) {
+
         model.addAttribute("libro", libroService.listarId(id_libro));
-        return "/views/libro/editar_libro";
+
+        return "views/libro/editar_libro";
     }
 
+
     @RequestMapping("/delete/{id_libro}")
-    public String deleteLibro(@PathVariable(name = "id_libro") int id_libro, RedirectAttributes attribute) {
+    public String deleteLibro(
+            @PathVariable(name="id_libro") int id_libro,
+            RedirectAttributes attribute) {
+
+
         try {
+
             libroService.delete(id_libro);
-            attribute.addFlashAttribute("info", "Libro eliminado correctamente");
-        } catch (Exception e) {
-            attribute.addFlashAttribute("error", "No se pudo eliminar el libro");
+
+            attribute.addFlashAttribute("info",
+                    "Libro eliminado correctamente");
+
+
+        } catch(Exception e) {
+
+            attribute.addFlashAttribute("error",
+                    "No se pudo eliminar el libro");
         }
+
+
         return "redirect:/views/libro/";
     }
 }
